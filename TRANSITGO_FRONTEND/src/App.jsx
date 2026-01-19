aa6f06840d8aa6533e01c0e3b5049ddfc87d2dc4
Ravindu Kolambage <143486662+Ravindukolambage@users.noreply.github.com>
2026-01-19 18:04:07 +0530

    create signup page

--- TRANSITGO_FRONTEND/src/App.jsx
+++ TRANSITGO_FRONTEND/src/App.jsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from './components/layouts/MainLayout';
-import BusRegistration from './pages/registration/BusRegistration';
+import MainLayout from "./components/layouts/MainLayout";
+import BusRegistration from "./pages/registration/BusRegistration";
+import Signup from "./pages/auth/Signup";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainLayout />} >
-        <Route path='bus_registration' element={<BusRegistration />} />
+        {/* Public auth routes */}
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Main app layout routes */}
+        <Route path="/" element={<MainLayout />}>
+          <Route path="bus_registration" element={<BusRegistration />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
