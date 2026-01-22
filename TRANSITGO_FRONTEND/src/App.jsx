b71e4165fbddca7c1832f0755b32993659b25545
Ravindu Kolambage <143486662+Ravindukolambage@users.noreply.github.com>
2026-01-22 10:29:21 +0530

    create sign in page and update sign up page

--- TRANSITGO_FRONTEND/src/App.jsx
+++ TRANSITGO_FRONTEND/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/layouts/MainLayout";
 import BusRegistration from "./pages/registration/BusRegistration";
 import Signup from "./pages/auth/Signup";
+import Signin from "./pages/auth/Signin";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         {/* Public auth routes */}
         <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
 
         {/* Main app layout routes */}
         <Route path="/" element={<MainLayout />}>
